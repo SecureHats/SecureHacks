674ebef6fc8551c431a3e1f7caea146c3a4a72ca
Rogier Dijkman <r.dijkman@nextfence.nl>
2022-01-29 16:44:23 +0100

    Added logpath

--- scripts/Azure/Sentinel/Enable-AlertRules/Enable-AlertRules.ps1
+++ scripts/Azure/Sentinel/Enable-AlertRules/Enable-AlertRules.ps1
@@ -12,22 +12,22 @@ function Enable-AlertRules {
     Param
     (
         # Graph access token
-        [Parameter(Mandatory = $true, ValueFromPipeline=$true, Position = 0)]
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true, Position = 0)]
         [string]$ResourceGroupName,
 
-        [Parameter(Mandatory = $true, ValueFromPipeline=$true, Position = 1)]
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true, Position = 1)]
         [string]$WorkspaceName,
 
-        [Parameter(Mandatory = $false, ValueFromPipeline=$true, Position = 2)]
+        [Parameter(Mandatory = $false, ValueFromPipeline = $true, Position = 2)]
         [switch]$UseWatchList,
 
-        [Parameter(Mandatory = $false, ValueFromPipeline=$true, Position = 3)]
+        [Parameter(Mandatory = $false, ValueFromPipeline = $true, Position = 3)]
         [string]$WatchlistName = 'ActiveConnectors',
 
-        [Parameter(Mandatory = $false, ValueFromPipeline=$true, Position = 4)]
+        [Parameter(Mandatory = $false, ValueFromPipeline = $true, Position = 4)]
         [switch]$Override,
 
-        [Parameter(Mandatory = $false, ValueFromPipeline=$true, Position = 5)]
+        [Parameter(Mandatory = $false, ValueFromPipeline = $true, Position = 5)]
         [ValidateSet('AlsidForAD',
             'AWS',
             'AzureActiveDirectory',
@@ -73,7 +73,10 @@ function Enable-AlertRules {
             'WindowsSecurityEvents',
             'Zscaler'
         )]
-        [array]$DataConnectors
+        [array]$DataConnectors,
+
+        [Parameter(Mandatory = $false, ValueFromPipeline = $true, Position = 6)]
+        [string]$logFileLocation = $($env:USERPROFILE)
     )
 
     $context = Get-AzContext
@@ -90,7 +93,7 @@ function Enable-AlertRules {
     }
 
     $SubscriptionId = $context.Subscription.Id
-    $logFile = '{0}\failed-rules-{1}.json' -f $($env:USERPROFILE), (Get-Date -f yyyyMMdd-hhmm)
+
     $logo = "
      _____                           __  __      __
     / ___/___  _______  __________  / / / /___ _/ / ____
@@ -100,30 +103,43 @@ function Enable-AlertRules {
 
     Clear-Host
     Write-Host $logo -ForegroundColor White
-    $_details = [ordered]@{
-        'Subscription Id'         = $($context.Subscription.id)
-        'Log Analytics workspace' = $($WorkspaceName)
-        'Data Connectors'         = $($DataConnectors)
-        'Logfile path'            = $($logFile)
+
+    if ($logFileLocation) {
+        if (!(Test-Path $logFileLocation)) {
+            $null = New-Item -ItemType "directory" -Path $logFileLocation
+        }
+        $logFile = '{0}\failed-rules-{1}.json' -f $($logFileLocation), (Get-Date -f yyyyMMdd-hhmm)
+    } else {
+        $logFile = '{0}\failed-rules-{1}.json' -f $($env:USERPROFILE), (Get-Date -f yyyyMMdd-hhmm)
     }
 
-    Write-Output $_details "`n`n"
+    $workspace = Get-AzResource -Name $WorkspaceName -ResourceType 'Microsoft.OperationalInsights/workspaces'
+
+    if ($null -ne $workspace) {
+        $apiVersion = '?api-version=2021-10-01-preview'
+        $baseUri = '{0}/providers/Microsoft.SecurityInsights' -f $workspace.ResourceId
+        $watchlist = '{0}/watchlists/{1}/watchlistItems{2}' -f $baseUri, $WatchlistName, $apiVersion
+        $templatesUri = '{0}/alertRuleTemplates{1}' -f $baseUri, $apiVersion
+        $alertUri = '{0}/alertRules' -f $baseUri
+    }
+    else {
+        Write-Output "[-] Unable to retrieve log Analytics workspace"
+        exit
+    }
+
     Write-Verbose ($_context | ConvertTo-Json)
 
-    $apiVersion = '?api-version=2021-10-01-preview'
-    $baseUri = "/subscriptions/${SubscriptionId}/resourceGroups/${ResourceGroupName}/providers/Microsoft.OperationalInsights/workspaces/${WorkspaceName}"
-    $watchlist = "$baseUri/providers/Microsoft.SecurityInsights/watchlists/$WatchlistName/watchlistItems$apiVersion"
-    $templatesUri = "$baseUri/providers/Microsoft.SecurityInsights/alertRuleTemplates$apiVersion"
-    $alertUri = "$baseUri/providers/Microsoft.SecurityInsights/alertRules"
     $alertRulesTemplates = @()
     $i = 0
 
     if ($UseWatchList) {
         $_content = ((Invoke-AzRestMethod -Path "$watchlist" -Method GET).content | ConvertFrom-Json).value
         if (-not($_content)) {
-            Write-Error "Unable to find watchlist [$($watchlistName)] make sure it exists"
-            break
-        } else {
+            Write-Host "[-] Unable to find watchlist [$($watchlistName)] make sure to use the Alias name`n"
+            Write-Host "[-] The watchlist Alias name is Case Sensitive !"
+            exit
+        }
+        else {
             $watchlistItems = $_content.properties.itemsKeyValue
             $DataConnectors = ($watchlistItems | Where-Object Enabled -EQ 'True').connectorName
         }
@@ -131,16 +147,25 @@ function Enable-AlertRules {
 
     if (-not($DataConnectors)) {
         $alertRulesTemplates = ((Invoke-AzRestMethod -Path "$($templatesUri)" -Method GET).Content | ConvertFrom-Json).value
-    } else {
+    }
+    else {
         $templates = ((Invoke-AzRestMethod -Path "$($templatesUri)" -Method GET).Content | ConvertFrom-Json).value
         foreach ($connector in $DataConnectors) {
             $alertRulesTemplates += ($templates | Where-Object { $_.Properties.RequiredDataConnectors.connectorId -contains $connector -and $_.kind -eq 'Scheduled' })
         }
     }
 
+    $_details = [ordered]@{
+        'Subscription Id'         = $($SubscriptionId)
+        'Log Analytics workspace' = $($WorkspaceName)
+        'Data Connectors'         = $($DataConnectors)
+        'Logfile path'            = $($logFile)
+    }
+    Write-Output $_details "`n`n"
+
     Write-Output "[+] Searching for duplicate rule templates"
-    $duplicateRules = ($alertRulesTemplates.properties.displayname | Group-Object | Where-Object {$_.Count -gt 1})
-    $uniqueRules = ($alertRulesTemplates.properties.displayname | Group-Object | Where-Object {$_.Count -eq 1})
+    $duplicateRules = ($alertRulesTemplates.properties.displayname | Group-Object | Where-Object { $_.Count -gt 1 })
+    $uniqueRules = ($alertRulesTemplates.properties.displayname | Group-Object | Where-Object { $_.Count -eq 1 })
 
     if ($null -ne $duplicateRules) {
         Write-Output "[+] Detected $($duplicateRules.count) duplicate rule templates"
@@ -154,45 +179,47 @@ function Enable-AlertRules {
             Write-Verbose "$($item.properties.displayName)"
             $alertUriGuid = $alertUri + '/' + $($item.name) + $apiVersion
             $i++
-                Write-Host "[+] Processing $($i) of $($uniqueRules.count) : $($item.properties.displayname)" -ForegroundColor Green
-                $properties = @{
-                    queryFrequency        = $item.properties.queryFrequency
-                    queryPeriod           = $item.properties.queryPeriod
-                    triggerOperator       = $item.properties.triggerOperator
-                    triggerThreshold      = $item.properties.triggerThreshold
-                    severity              = $item.properties.severity
-                    query                 = $item.properties.query
-                    entityMappings        = $item.properties.entityMappings
-                    templateVersion       = $item.properties.version
-                    displayName           = $item.properties.displayName
-                    description           = $item.properties.description
-                    enabled               = $true
-                    suppressionDuration   = 'PT5H'
-                    suppressionEnabled    = $false
-                    alertRuleTemplateName = $item.name
-                }
+            Write-Host "[+] Processing $($i) of $($uniqueRules.count) : $($item.properties.displayname)" -ForegroundColor Green
+            $properties = @{
+                queryFrequency        = $item.properties.queryFrequency
+                queryPeriod           = $item.properties.queryPeriod
+                triggerOperator       = $item.properties.triggerOperator
+                triggerThreshold      = $item.properties.triggerThreshold
+                severity              = $item.properties.severity
+                query                 = $item.properties.query
+                entityMappings        = $item.properties.entityMappings
+                templateVersion       = $item.properties.version
+                displayName           = $item.properties.displayName
+                description           = $item.properties.description
+                enabled               = $true
+                suppressionDuration   = 'PT5H'
+                suppressionEnabled    = $false
+                alertRuleTemplateName = $item.name
+            }
 
-                if (-not($Override -and ($item.properties.alertRulesCreatedByTemplateCount -ne 0))) {
-                    $properties.displayName = $item.properties.displayName
-                } else {
-                    $properties.displayName = "[COPY] - $($item.properties.displayName)"
-                    $alertUriGuid = $alertUri + '/' + $($alertName) + $apiVersion
-                }
+            if (-not($Override -and ($item.properties.alertRulesCreatedByTemplateCount -ne 0))) {
+                $properties.displayName = $item.properties.displayName
+            }
+            else {
+                $properties.displayName = "[COPY] - $($item.properties.displayName)"
+                $alertUriGuid = $alertUri + '/' + $($alertName) + $apiVersion
+            }
 
-                if ($item.properties.techniques) {
-                    $properties.techniques = $item.properties.techniques
-                }
-                if ($item.properties.tactics) {
-                    $properties.tactics = $item.properties.tactics
-                }
+            if ($item.properties.techniques) {
+                $properties.techniques = $item.properties.techniques
+            }
+            if ($item.properties.tactics) {
+                $properties.tactics = $item.properties.tactics
+            }
 
-                $alertBody = @{}
-                $alertBody | Add-Member -NotePropertyName kind -NotePropertyValue $item.kind -Force
-                $alertBody | Add-Member -NotePropertyName properties -NotePropertyValue $properties
+            $alertBody = @{}
+            $alertBody | Add-Member -NotePropertyName kind -NotePropertyValue $item.kind -Force
+            $alertBody | Add-Member -NotePropertyName properties -NotePropertyValue $properties
             if (-not($Override -or ($item.properties.alertRulesCreatedByTemplateCount -eq 0))) {
-                Write-Host '[-] WARNING: Schedules rule already exists. Use the [-Override] switch to create a duplicate rule.' -ForegroundColor Yellow
+                Write-Host '[-] Warning: Schedules rule already exists. Use the [-Override] switch to create a duplicate rule.' -ForegroundColor Red
                 break
-            } else {
+            }
+            else {
                 try {
                     $result = Invoke-AzRestMethod -Path $alertUriGuid -Method PUT -Payload ($alertBody | ConvertTo-Json -Depth 10)
 
@@ -202,8 +229,9 @@ function Enable-AlertRules {
                             Write-Verbose 'Rule was not created from template, recreating rule'
                             Invoke-AzRestMethod -Path $alertUriGuid -Method DELETE
                             Invoke-AzRestMethod -Path $alertUriGuid -Method PUT -Payload ($alertBody | ConvertTo-Json -Depth 10)
-                        } else {
-                            Write-Host 'Warning: '(($result.Content | ConvertFrom-Json).error.message) -ForegroundColor Red
+                        }
+                        else {
+                            Write-Host '[-] Warning: '(($result.Content | ConvertFrom-Json).error.message) -ForegroundColor Red
                             $currentItem = [PSCustomObject]@{
                                 'ruleName'  = $item.properties.displayNAme
                                 'tactic'    = $item.properties.tactics
@@ -213,7 +241,8 @@ function Enable-AlertRules {
                             $currentItem | ConvertTo-Json | Out-File $logFile -Append
                         }
                     }
-                } catch {
+                }
+                catch {
                     Write-Verbose $_
                     Write-Error "Unable to create alert rule with error code: $($_.Exception.Message)" -ErrorAction Stop
                 }
